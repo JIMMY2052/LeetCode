3cf6a816c0511b83add7e5d3efc6fa67a4afc861
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-01-29 17:51:30 +0800

    Time: 549 ms (51.99%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -3,5 +3,4 @@ SELECT DISTINCT customer_id
 FROM customer c JOIN
 product p ON c.product_key = p.product_key
 GROUP BY c.customer_id
-HAVING COUNT(DISTINCT p.product_key) =
-       (SELECT COUNT(*) FROM product);
+HAVING count(distinct p.product_key) = (SELECT COUNT(*) FROM product)
