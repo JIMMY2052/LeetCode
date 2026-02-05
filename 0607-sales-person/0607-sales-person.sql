8537c6d92390582f99487840385d674d56bcf7a4
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-02-05 23:14:27 +0800

    Time: 1257 ms (77.55%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,7 +1,2 @@
-SELECT name
-FROM salesperson
-WHERE sales_id not in (SELECT sales_id
-FROM orders
-WHERE com_id = (SELECT com_id
-FROM company
-WHERE name = "Red"))
+select name from salesperson
+where sales_id not in (select sales_id from orders where com_id = (select com_id from company where name = "red"))
