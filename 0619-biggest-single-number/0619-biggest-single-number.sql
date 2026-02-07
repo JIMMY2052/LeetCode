ce0c6e0d9ff7e592130d704ac01338f83e6ddf23
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-02-08 00:34:04 +0800

    Time: 399 ms (67.65%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,2 +1,6 @@
-# Write your MySQL query statement below
-SELECT MAX(num)  as num FROM (SELECT count(*) as cnt,num FROM mynumbers GROUP BY num) as t WHERE cnt = 1
+with cte as(
+select num as num
+from MyNumbers
+group by num
+having count(1) = 1)
+select max(num) as num from cte;
