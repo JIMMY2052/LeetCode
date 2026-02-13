f1f06ecc239a29c38cca7df72b718e6c4ed9ff35
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-02-13 15:30:17 +0800

    Time: 666 ms (11.95%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,7 @@
+with temp as (
+select * , count(primary_flag)over(partition by employee_id ) as cnt
+from Employee)
+
+select employee_id , department_id
+from temp
+where (cnt = 1) or (cnt >1 and primary_flag = 'Y')
