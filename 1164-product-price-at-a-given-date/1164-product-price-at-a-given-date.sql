bb130501f3383fb9a4edc95fd7b0cae2d5f28184
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-02-18 15:33:05 +0800

    Time: 644 ms (26.2%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+
+
+with cte as (SELECT product_id, new_price, row_number() over (partition by product_id order by change_date desc) as 'num' FROM products WHERE change_date <= '2019-08-16'),
+p as (SELECT product_id FROM products GROUP BY product_id)
+
+
+
+SELECT p.product_id, coalesce(t.new_price,10) as price
+FROM (SELECT product_id FROM p) p LEFT JOIN  (SELECT product_id, new_price FROM cte WHERE num =1) t
+ON p.product_id = t.product_id
+
+
+
