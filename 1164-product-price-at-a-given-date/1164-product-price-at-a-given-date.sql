e1ee9b518179aeeb6e383428b7e0e7b91f4c4284
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-02-18 15:36:17 +0800

    Time: 438 ms (80.11%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -3,9 +3,7 @@
 
 
 with cte as (SELECT product_id, new_price, row_number() over (partition by product_id order by change_date desc) as 'num' FROM products WHERE change_date <= '2019-08-16'),
-p as (SELECT product_id FROM products GROUP BY product_id)
-
-
+p as (SELECT DISTINCT product_id FROM products)
 
 SELECT p.product_id, coalesce(t.new_price,10) as price
 FROM (SELECT product_id FROM p) p LEFT JOIN  (SELECT product_id, new_price FROM cte WHERE num =1) t
