51a5a0bc164d0fd4c53b88db0a271d97c0c8e0d8
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-03-28 22:15:55 +0800

    Time: 620 ms (41.13%), Space: 0B (100%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -1,15 +1,23 @@
-with cte as(SELECT  p1.product_id as p1, p2.product_id as p2
-FROM ProductPurchases p1 
-JOIN ProductPurchases p2 ON p1.user_id = p2.user_id and P1.product_id < p2.product_id)
-,cta as (SELECT *, count(*) as 'customer_count'
-FROM cte 
-GROUP BY p1,p2)
+WITH cte as 
+    (   
+        SELECT
+            p1.product_id as product1_id, 
+            p2.product_id as product2_id,
+            COUNT(p1.user_id) as customer_count
+        FROM ProductPurchases as p1 LEFT JOIN ProductPurchases as p2 ON p1.user_id=p2.user_id
+        WHERE p1.product_id<p2.product_id
+        GROUP BY p1.product_id, p2.product_id
+        HAVING COUNT(p1.user_id)>=3
+    )
 
-SELECT p1 as product1_id, p2 as product2_id, (SELECT category  FROM productInfo WHERE product_id = c.p1) as product1_category,
-(SELECT category FROM productInfo WHERE product_id = c.p2) as product2_category,
-customer_count 
-FROM cta c 
-WHERE customer_count >= 3
-ORDER BY customer_count desc, product1_id, product2_id
-
-
+SELECT 
+    product1_id,
+    product2_id,
+    p1.category as product1_category,
+    p2.category as product2_category,
+    customer_count
+FROM 
+    cte 
+    JOIN ProductInfo AS p1 on product1_id=p1.product_id
+    JOIN ProductInfo AS p2 on product2_id=p2.product_id
+ORDER BY customer_count DESC, product1_id ASC, product2_id ASC
