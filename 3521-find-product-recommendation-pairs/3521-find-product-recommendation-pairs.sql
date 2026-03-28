3f38044aed8b29fac366c4dee3e9ef7532b93216
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-03-28 22:13:44 +0800

    Time: 713 ms (17.6%), Space: 0B (100%) - LeetHub

---
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -0,0 +1,24 @@
+-- with cte as (SELECT user_id, product_id
+-- FROM productPurchases 
+-- GROUP BY user_id, product_id)
+
+-- SELECT product_id as p1, lead(product_id) over(partition by user_id) as p2
+-- FROM cte 
+
+
+with cte as(SELECT  p1.product_id as p1, p2.product_id as p2
+FROM ProductPurchases p1 
+JOIN ProductPurchases p2 ON p1.user_id = p2.user_id and P1.product_id < p2.product_id)
+
+,cta as (SELECT *, count(*) as 'customer_count'
+FROM cte 
+GROUP BY p1,p2)
+
+SELECT p1 as product1_id, p2 as product2_id, (SELECT category  FROM productInfo WHERE product_id = c.p1) as product1_category,
+(SELECT category FROM productInfo WHERE product_id = c.p2) as product2_category,
+customer_count 
+FROM cta c 
+WHERE customer_count >= 3
+ORDER BY customer_count desc, product1_id, product2_id
+
+
