cefa12790487416fcc3308a10af7edefb5dad144
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-03-30 21:14:50 +0800

    Time: 556 ms (93.76%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -2,7 +2,7 @@
 with cte as (SELECT *, row_number() OVER(partition by customer_id ORDER BY order_date) as rn
 FROM delivery)
 
-SELECT  ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END) / COUNT(*) * 100,2)as immediate_percentage 
+SELECT  ROUND(avg(order_date= customer_pref_delivery_date) * 100,2)as immediate_percentage 
 FROM cte
 WHERE rn = 1
 
