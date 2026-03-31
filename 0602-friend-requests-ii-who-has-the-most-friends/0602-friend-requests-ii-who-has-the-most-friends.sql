d59c0707681dd4e4e1f4e922fb5d08b2dfe080e7
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-03-31 23:10:16 +0800

    Time: 343 ms (15.15%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,7 +1,25 @@
-select requester_id as id
-    from requestaccepted
+# Write your MySQL query statement below
+with cta as (SELECT requester_id as id
+FROM requestAccepted
+UNION 
+SELECT accepter_id
+FROM requestAccepted
+),
+cte as (
+SELECT *
+FROM cta c 
+JOIN requestAccepted r1 ON c.id = r1.requester_id
+UNION 
+SELECT *
+FROM cta c 
+JOIN requestAccepted r2 ON c.id = r2.accepter_id
+),
+c as(
+SELECT  id, count(*) as num
+FROM cte
+GROUP BY id
+)
 
-    union all
+SELECT * FROM c ORDER BY num desc limit 1
 
-    select accepter_id as id
-    from requestaccepted
+
