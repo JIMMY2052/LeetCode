3365a89255e3fca7f9831d6a4359975a83fbf501
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-03-31 23:10:41 +0800

    Time: 556 ms (51.1%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,3 +1,14 @@
-select concat(lat,lon) as location from Insurance
-group by concat(lat,lon)
-having count(pid) = 1
+select round(sum(tiv_2016), 2) tiv_2016
+from insurance
+where tiv_2015 in (
+select tiv_2015
+from insurance
+group by tiv_2015
+having count(tiv_2015)>1
+)
+and (lat, lon) in (
+select lat, lon
+from insurance
+group by lat + lon
+having count(lat + lon)=1
+)
