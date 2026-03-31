5d72e9fea93ec511e5bc7bac12a201515609fa2f
Jimmy Chan <jimmychankahlok66@gmail.com>
2026-03-31 23:10:32 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,14 +1,3 @@
-select round(sum(tiv_2016), 2) tiv_2016
-from insurance
-where tiv_2015 in (
-select tiv_2015
-from insurance
-group by tiv_2015
-having count(tiv_2015)>1
-)
-and (lat, lon) in (
-select lat, lon
-from insurance
-group by lat + lon
-having count(lat + lon)=1
-)
+select concat(lat,lon) as location from Insurance
+group by concat(lat,lon)
+having count(pid) = 1
